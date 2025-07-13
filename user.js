9606304d3879d4b681e6ae61932ba63c28a43616
Lorea De La Torre <loreataina@icloud.com>
2025-07-13 00:05:27 -0400

    done done

--- user.js
+++ user.js
@@ -3,36 +3,22 @@ const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
   const id = event.target.value;
+  renderPosts(id);
+}
+
+async function renderPosts(id) {
   const posts = await fetch(
     `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
   const postsData = await posts.json();
-  postListEl.innerHTML = postsData
-    .map(
-      (post) =>
-        `<div class="post">
-        <div class="post__title">${post.title}</div>
-        <p class="post__body">${post.body}</p>
-      </div>`
-    )
-    .join("");
+  postListEl.innerHTML = postsData.map((post) => postHTML(post)).join("");
 }
 
-async function renderPosts() {
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
-  const postsData = await posts.json();
-
-  postListEl.innerHTML = postsData.map((post) => postHTML).join("");
-}
-
-function postHTML() {
-  return;
-  `<div class="post">
+function postHTML(post) {
+  return `<div class="post">
         <div class="post__title">${post.title}</div>
         <p class="post__body">${post.body}</p>
       </div>`;
 }
 
-renderPosts();
+renderPosts(id);
